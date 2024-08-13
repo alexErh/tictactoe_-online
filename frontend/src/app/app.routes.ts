9569d9f23702f00a517cf7b5e4f786ce43292fdb
DuniaHamad <dunia.hamad@mni.thm.de>
2024-08-13 21:57:15 +0200

    Route anpassen

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { MatchmakingQueueComponent } from './matchmaking-queue/matchmaking-queue.component';
 import { SpielseiteComponent } from './spielseite/spielseite.component';
+import { ProfilseiteComponent } from './profilseite/profilseite.component';
 
 export const routes: Routes = [
 
@@ -11,6 +12,7 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'matchmaking', component: MatchmakingQueueComponent },
   { path: 'game', component: SpielseiteComponent },
+  { path: 'profil', component: ProfilseiteComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 
 ];
