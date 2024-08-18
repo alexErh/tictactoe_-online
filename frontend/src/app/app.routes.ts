765648fa1ed4a64fecf1f38c1d261947fe8268c3
rouatekaya8 <rouabentekaya@gmail.com>
2024-08-18 10:22:26 +0100

    Spielseite

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
+import { SpielseiteComponent} from './spielseite/spielseite.component';
 
 export const routes: Routes = [
 
   { path: 'register', component: RegistrationComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'game', component: SpielseiteComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
+
 
 ];
 @NgModule({
--- frontend/src/app/spielseite/spielseite.component.ts
+++ frontend/src/app/spielseite/spielseite.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './spielseite.component.css'
 })
 export class SpielseiteComponent {
-
+  board: string[][] = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ];
+  currentPlayer: string = 'X';
+  makeMove(row: number, col: number) {
+    if (!this.board[row][col]) {
+      this.board[row][col] = this.currentPlayer;
+      this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+    }
+  }
 }
