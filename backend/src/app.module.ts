ea005c9bdd581fd214c39f2101a02bffa6382c00
DuniaHamad <dunia.hamad@mni.thm.de>
2024-09-03 22:28:38 +0200

    Test

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,28 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {TypeOrmModule} from "@nestjs/typeorm";
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import {ConfigModule} from "@nestjs/config";
-import {User} from "./database/tables/User";
+//import {ConfigModule} from "@nestjs/config";
+import { User } from './database/tables/User';
 import { MatchGateway } from './match/match.gateway';
 import { MatchService } from './match/match.service';
 import { MatchmakingController } from './match/matchmaking/matchmaking.controller';
 
 @Module({
   imports: [
-      TypeOrmModule.forRoot({
-        type: 'sqlite',
-        database: './tmp.sqlite',
-        entities: [User],
-        synchronize: true,
-        extra: {
-          busyTimeout: 5000, // 5 Sekunden
-        },
-      }),
-      AuthModule, 
-      UsersModule, //GameModule
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './tmp.sqlite',
+      entities: [User],
+      synchronize: true,
+      extra: {
+        busyTimeout: 5000, // 5 Sekunden
+      },
+    }),
+    AuthModule,
+    UsersModule, //GameModule
   ],
   controllers: [AppController, MatchmakingController],
   providers: [AppService, MatchGateway, MatchService],
