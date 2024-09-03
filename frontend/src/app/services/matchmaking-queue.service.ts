ca852ac2e8f4d7911296ccb6a44bbc00175054e6
DuniaHamad <dunia.hamad@mni.thm.de>
2024-09-03 21:06:54 +0200

    14-matchmaking queue - Websocket client und server seitig funktioniert

--- frontend/src/app/services/matchmaking-queue.service.ts
+++ frontend/src/app/services/matchmaking-queue.service.ts
@@ -1,18 +1,74 @@
 import { Injectable } from '@angular/core';
-import { WebsocketService } from './websocket.service';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { io, Socket } from 'socket.io-client';
+
+export interface Player {
+  id?: string;
+  nickname: string;
+  elo: number;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MatchmakingQueueService {
+  private socket: Socket;
+  // den aktuellen Status eines gefundenen Gegners speichert
+  private matchFound$ = new BehaviorSubject<Player | null>(null);
 
-  constructor(private socket: WebsocketService) {}
-
-  joinQueue(player: any) {
-    this.socket.emit('joinQueue', player);
+  constructor() {
+    this.connect();
+    //Registriert einen Listener für das matchFound-Ereignis.
+    this.socket.on('matchFound', (data: Player) => {
+      //Wenn ein Gegner gefunden wird, wird das matchFound$-Subject mit den Spieldaten aktualisiert.
+      this.matchFound$.next(data);
+    });
   }
 
-  onMatchFound() {
-    return this.socket.fromEvent('matchFound');
+  connect() {
+    this.socket = io('http://localhost:3000');
+  }
+
+  emit(event: string, data: any) {
+    this.socket.emit(event, data);
+  }
+
+  //listen: Gibt ein Observable zurück, das ein Ereignis vom Server abonniert. Wenn das Ereignis eintritt, wird der Observer mit den empfangenen Daten benachrichtigt.
+  // Die Rückgabefunktion sorgt dafür, dass der Listener entfernt wird, wenn das Observable unsubscribed wird.
+  listen(event: string): Observable<any> {
+    return new Observable((observer) => {
+      this.socket.on(event, (data) => {
+        observer.next(data);
+      });
+      return () => this.socket.off(event);
+    });
+  }
+
+  disconnect() {
+    if (this.socket) {
+      this.socket.disconnect();
+    }
+  }
+
+  joinQueue(playerData: { nickname: string; elo: number }) {
+    this.socket.emit('joinQueue', playerData);
+  }
+
+  cancelQueue() {
+    this.socket.emit('cancelQueue');
+    this.matchFound$.next(null);
+  }
+
+  //onMatchFound: Gibt ein Observable zurück, das auf das matchFound-Ereignis wartet. Wenn ein Gegner gefunden wird, wird der Wert von matchFound$ entsprechend aktualisiert.
+  onMatchFound(): Observable<Player | null> {
+    return this.listen('matchFound').pipe(
+      tap((opponent) => {
+        this.matchFound$.next(opponent);
+      })
+    );
+  }
+
+  getMatchFound(): Observable<Player | null> {
+    return this.matchFound$.asObservable();
   }
 }
