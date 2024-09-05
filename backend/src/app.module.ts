097c5c0c4b082006f2892d62ad27752a52e47144
DuniaHamad <dunia.hamad@mni.thm.de>
2024-09-05 08:07:17 +0200

    Test

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,23 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {TypeOrmModule} from "@nestjs/typeorm";
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import {ConfigModule} from "@nestjs/config";
-import {User} from "./database/tables/User";
-import { GameModule } from './game/game.module';
+import { ConfigModule } from '@nestjs/config';
+import { User } from './database/tables/User';
+//import { GameModule } from './game/game.module';
 
 @Module({
   imports: [
-      TypeOrmModule.forRoot({
-        type: 'sqlite',
-        database: './tmp.sqlite',
-        entities: [User],
-        synchronize: true,
-      }),
-      AuthModule, 
-      UsersModule, GameModule
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './tmp.sqlite',
+      entities: [User],
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule, //GameModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,24 +1,27 @@
-import {Injectable, UnauthorizedException} from '@nestjs/common';
-import {UsersService} from "../users/users.service";
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
 import { SessionData } from 'express-session';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService) {}
 
-    async signIn(username: string, pass: string, session: SessionData): Promise<void> {
-        const user = await this.usersService.getOne(username);
-        console.log("user", user);
-        if (user?.password !== pass) {
-            throw new UnauthorizedException();
-        }
-        session.isLoggedIn = true;
-        session.user = { nickname: user.nickname };
+  async signIn(
+    username: string,
+    pass: string,
+    session: SessionData,
+  ): Promise<void> {
+    const user = await this.usersService.getOne(username);
+    console.log('user', user);
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
     }
+    session.isLoggedIn = true;
+    session.user = { nickname: user.nickname };
+  }
 
-    signOut(session: SessionData): void {
-        session.isLoggedIn = undefined;
-        delete session.user;
-    }
-
+  signOut(session: SessionData): void {
+    session.isLoggedIn = undefined;
+    delete session.user;
+  }
 }
