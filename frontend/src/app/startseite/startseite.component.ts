4f44f769592837da82ef92b0f34dc6a690f2c6c9
DuniaHamad <dunia.hamad@mni.thm.de>
2024-09-12 09:23:54 +0200

    42-Einfügen-der-Startseite-und-Ergebnisseite-in-frontend

--- frontend/src/app/startseite/startseite.component.ts
+++ frontend/src/app/startseite/startseite.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import { NavigationComponent } from '../navigation/navigation.component';
 
 @Component({
   selector: 'app-startseite',
   standalone: true,
-  imports: [],
-  templateUrl: './startseite.component.html',
+  imports: [
+    NavigationComponent,
+  ],  templateUrl: './startseite.component.html',
   styleUrl: './startseite.component.css'
 })
 export class StartseiteComponent {
 
+  playerElo: number = 1000;
+
+  constructor(private router: Router) {}
+
+  playNow(): void {
+    console.log("Spieler in die Matchmaking-Queue eingetreten.");
+    this.router.navigate(['/matchmaking']);
+  }
 }
